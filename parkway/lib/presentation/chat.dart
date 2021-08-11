7946fb19dba1eff6f0d1ba7969b9f4f6a4735cfa
Akintunde Segun <akintundeayofesegun@gmail.com>
2021-08-11 14:59:58 -0700

    added required assets to the project

---
+++ parkway/lib/presentation/chat.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+
+
+      ),
+    );
+  }
+}
