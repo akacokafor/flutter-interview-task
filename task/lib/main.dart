a070b1257189ba59c9087ae530f59d1d36d0d86a
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-09 13:37:12 +0100

    Clean up

--- task/lib/main.dart
+++ task/lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:task/config/size-config.dart';
 import 'package:task/constants/app-routes.dart';
-import 'package:task/views/book_a_walk/screen.dart';
-import 'package:task/views/chat/screen.dart';
-import 'package:task/views/home/screen.dart';
-import 'package:task/views/onboarding/screen.dart';
-import 'package:task/views/register/screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,7 +22,6 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Flutter Interview Task',
           debugShowCheckedModeBanner: false,
-          // home: OnboardingScreen(),
           routes: Routes.routes()
         );
       });
